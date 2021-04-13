69620fffe81f4332fec52d2ef0f1c246accf2180
IvanMi773 <imihailovskij7@gmail.com>
2021-04-13 19:14:25 +0300

    add forex service

---
+++ fortex-service/src/main/java/com/example/fortex_service/repository/ExchangeValueRepository.java
@@ -0,0 +1,8 @@
+package com.example.fortex_service.repository;
+
+import com.example.fortex_service.model.ExchangeValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+    ExchangeValue findByFromAndTo(String from, String to);
+}
